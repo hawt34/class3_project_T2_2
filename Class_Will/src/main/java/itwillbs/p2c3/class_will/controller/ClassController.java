1fc146670b6b1d129f93c15db80634eac5f75a9e
김현경 <@DESKTOP-6TH2HJB>
2024-06-11 03:49:57 +0900

    클래스 디테일

---
+++ Class_Will/src/main/java/itwillbs/p2c3/class_will/controller/ClassController.java
@@ -0,0 +1,14 @@
+package itwillbs.p2c3.class_will.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClassController {
+	
+	// 클래스 디테일
+	@GetMapping("Class/Detail")
+	public String classDetail() {
+		return"class/class_detail";
+	}
+}

a60660b1b9d95e31cf4d0cd9bb82c4bf00a8bb52
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-06-10 17:56:50 +0900

    전준혁 - ㅁㅁㄴㅇ

--- Class_Will/src/main/java/itwillbs/p2c3/class_will/controller/AdminController.java
+++ Class_Will/src/main/java/itwillbs/p2c3/class_will/controller/AdminController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class AdminController {
 	
 	
 	@GetMapping("test2")
@@ -18,4 +18,12 @@ public class TestController {
 		
 		return "admin/admin_member";
 	}
+	
+	@GetMapping("test4")
+	public String test4() {
+		
+		return "admin/admin_class";
+	}
+	
+	
 }

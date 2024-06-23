3e204fa7a70a5431f19744d751a97018adaa0bc6
jyojyo <jyojyo@jyo-PC>
2024-06-24 07:23:49 +0900

    수정

--- Class_Will/src/main/java/itwillbs/p2c3/class_will/MainController.java
+++ Class_Will/src/main/java/itwillbs/p2c3/class_will/MainController.java
@@ -1,5 +1,8 @@
 package itwillbs.p2c3.class_will;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 import itwillbs.p2c3.class_will.service.MainService;
 
@@ -19,6 +24,12 @@ public class MainController {
 	// 메인으로
 	@GetMapping("main")
 	public String main() {
+		
+		
+		
+		
+		
+		
 		return "main";
 	}
 	
@@ -46,15 +57,38 @@ public class MainController {
 	@GetMapping("top-field-category")
 	public String fieldCategory() {
 		
-		Map<String, String> fieldCateMap = mainService.selectFieldCate();
-		
-		
-		
-		
-		
-		
-		
-		
+	    List<Map<String, Object>> data = new ArrayList<>();
+	    Map<String, List<Map<String, Object>>> fieldCateMap = mainService.selectFieldCate();
+	    List<Map<String, Object>> bigCategory = fieldCateMap.get("bigCategory");
+	    List<Map<String, Object>> smallCategory = fieldCateMap.get("smallCategory");
+	    
+	    // 대분류에 소분류를 추가
+	    for (Map<String, Object> bcg : bigCategory) {
+	        Map<String, Object> map = new HashMap<>();
+	        String bigValue = (String) bcg.get("code_value");
+	        Integer common2_code = (Integer) bcg.get("common2_code"); // Integer로 변환
+	        map.put("id", common2_code);
+	        map.put("largeCategory", bigValue);
+	        map.put("hidden", bcg.get("code_hide").equals("N") ? false : true);
+	        
+	        // 소분류 데이터를 _children 배열에 추가
+	        List<Map<String, Object>> children = new ArrayList<>();
+	        for (Map<String, Object> scg : smallCategory) {
+	            Integer parent_code = (Integer) scg.get("common2_code"); // Integer로 변환
+	            if (common2_code.equals(parent_code)) {
+	                Map<String, Object> map2 = new HashMap<>();
+	                map2.put("id", scg.get("common3_code"));
+	                map2.put("largeCategory", bigValue);
+	                map2.put("smallCategory", scg.get("code_value"));
+	                map2.put("hidden", scg.get("code_hide").equals("N") ? false : true);
+	                children.add(map2);
+	            }
+	        }
+	        if (!children.isEmpty()) {
+	            map.put("_children", children);
+	        }
+	        data.add(map);
+	    }
 		
 		
 		
